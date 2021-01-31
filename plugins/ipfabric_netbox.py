3f48ad7ffa998d527842969e1dd4c990f4ec13be
Jeremy Schulman <jeremy.schulman@mlb.com>
2021-01-31 10:54:16 -0500

    update

---
+++ plugins/ipfabric_netbox.py
@@ -0,0 +1,40 @@
+from nauti.collections.devices import DeviceCollection
+from nauti.auditor import Auditor
+from nauti.tasks.reconile import Reconciler
+from nauti.collection import get_collection
+from nauti_ipfabric_netbox.devices import IPFabricNetboxDeviceCollectionReconciler
+
+
+@Reconciler.register('ipfabric', 'netbox', 'devices')
+class ReconcileIPFabricToNetboxDevices(IPFabricNetboxDeviceCollectionReconciler):
+
+    async def update_items(self):
+        # need to build the IPaddrs cache for the device update function to work properly
+        # TODO: review this approach
+        col_ipaddrs = get_collection(self.target.source, 'ipaddrs')
+        items = {
+            hostname: self.target.items[hostname]
+            for hostname in self.diff_res.changes
+        }
+
+        await col_ipaddrs.fetch_items(items=items)
+        col_ipaddrs.make_keys()
+        self.target.cache['ipaddrs'] = col_ipaddrs
+        await super().update_items()
+
+
+@Auditor.register('ipfabric', 'netbox', 'devices')
+class AuditIPFabricToNetboxDevices(Auditor):
+    fields = set(DeviceCollection.FIELDS) - {'model'}
+    key_fields = ('hostname', )
+
+    def origin_key_filter(self, item: dict):
+        # ignore Meraki, Cisco APs and Palo FW devices
+        if item['os_name'] in ['lap', 'pan-os', 'meraki']:
+            return False
+
+        return True
+
+    def target_key_filter(self, item: dict):
+        """ only include items that are present in the origin source """
+        return item['hostname'] in self.origin.items
---
+++ plugins/netbox_clearpass.py
@@ -0,0 +1,30 @@
+import json
+from nauti.auditor import Auditor
+
+
+@Auditor.register('netbox', 'clearpass', 'devices')
+class Nb2CPDevicesAuditor(Auditor):
+    fields = ('hostname', 'os_name', 'site', 'ipaddr')
+    key_fields = ('hostname', )
+
+    def origin_fetch_filter(self):
+        filter_params = {
+            'has_primary_ip': 'true'
+        }
+        filter_params.update(self.options.get('extras', {}))
+        return filter_params
+
+    def origin_key_filter(self, item: dict) -> bool:
+        if item['status'] not in ['active', 'offline', 'staged', 'planned']:
+            return False
+
+        if item['vendor'] == 'pan':
+            return False
+
+        return True
+
+    def target_fetch_filter(self):
+        if (site := self.options['extras'].get('site')) is not None:
+            return {'filter': json.dumps({'Location': site})}
+
+        return None
